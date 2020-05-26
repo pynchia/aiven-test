58f2f5c67694617b5a11ac13bff835152b3292f0
mbb <pynchia@gmail.com>
2020-05-26 18:26:05 +0200

    setup first stab

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+# with open('README.rst') as f:
+#     LONG_DESC = f.read()
+
+setup(
+    name='aiven',
+    version='0.0.1',
+    url='http://example.com',
+    description='Test exercise to produce and consume web metrics',
+    # long_description=LONG_DESC,
+    author='Pynchia', author_email='pyncha@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'],
+    keywords=['aiven', ],
+    # packages=find_packages(),
+    packages=['aiven'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points="""
+    [console_scripts]
+    producercli = aiven.producer.cli:cli
+    consumercli = aiven.consumer.cli:cli
+    """,
+    install_requires=[]
+)
